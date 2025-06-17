9e41219dcdb5bf637e6c74aaa2c4cae98fe7b34a
KennOtieno <otiken001@gmail.com>
2025-06-17 06:08:12 -0700

    Home Page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,55 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(20.0),    
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Jambo, Karibu Tena',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+
+                      Text(
+                        'Dear Pro',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Color(0xFF6C63FF).withOpacity(0.2),
+                    child: Icon(
+                      Icons.person,
+                      color: Color(0xFF6C63FF),
+                      size: 30,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+            ],
+          )
+        )
+      )
+    );
   }
 }
