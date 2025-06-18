002d89d4730c5682a79d43f548b2c7a90965e9c9
KennOtieno <otiken001@gmail.com>
2025-06-18 01:54:35 -0700

    Home Page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,23 +1,21 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage ({super.key});
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(20.0),    
-          ),
+          padding: const EdgeInsets.all(20.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Column(
+                  const Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
@@ -28,7 +26,6 @@ class HomePage extends StatelessWidget {
                         ),
                       ),
                       SizedBox(height: 5),
-
                       Text(
                         'Dear Pro',
                         style: TextStyle(
@@ -40,8 +37,8 @@ class HomePage extends StatelessWidget {
                   ),
                   CircleAvatar(
                     radius: 30,
-                    backgroundColor: Color(0xFF6C63FF).withOpacity(0.2),
-                    child: Icon(
+                    backgroundColor: const Color(0xFF6C63FF).withOpacity(0.2),
+                    child: const Icon(
                       Icons.person,
                       color: Color(0xFF6C63FF),
                       size: 30,
@@ -49,12 +46,12 @@ class HomePage extends StatelessWidget {
                   ),
                 ],
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               
               Container(
-                padding: EdgeInsets.all(20.0),
+                padding: const EdgeInsets.all(20.0),
                 decoration: BoxDecoration(
-                  gradient: LinearGradient(
+                  gradient: const LinearGradient(
                     colors: [
                       Color(0xFF6C63FF),
                       Color(0xFF5A54D1),
@@ -63,17 +60,85 @@ class HomePage extends StatelessWidget {
                   borderRadius: BorderRadius.circular(20.0),
                   boxShadow: [
                     BoxShadow(
-                      color: Colors.(0xFF6c63FF).withOpacity(0.3),
+                      color: const Color(0xFF6C63FF).withOpacity(0.3),
                       blurRadius: 15.0,
-                      offset: Offset(0, 10),
+                      offset: const Offset(0, 10),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Today\'s Mission',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(height: 15.0),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.local_fire_department,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                            SizedBox(width: 10.0),
+                            Text(
+                              'Burn 500 Calories',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    InkWell(
+                      onTap: () {
+                        // When tapped
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 8.0,
+                          horizontal: 15.0,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        child: const Row(
+                          children: [
+                            Icon(
+                              Icons.show_chart,
+                              color: Color(0xFF6C63FF),
+                              size: 20,
+                            ),
+                            SizedBox(width: 10.0),
+                            Text(
+                              'Track Progress',
+                              style: TextStyle(
+                                color: Color(0xFF6C63FF),
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ],
                 ),
               ),
             ],
-          )
-        )
-      )
+          ),
+        ),
+      ),
     );
   }
 }
