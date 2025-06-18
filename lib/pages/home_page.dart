b5fca8401b57f1d870f5451fc21c84b63d50762d
KennOtieno <otiken001@gmail.com>
2025-06-18 03:55:13 -0700

    Rejo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -92,7 +92,7 @@ class HomePage extends StatelessWidget {
                               'Burn 500 Calories',
                               style: TextStyle(
                                 color: Colors.white,
-                                fontSize: 18,
+                                fontSize: 13,
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
@@ -125,7 +125,7 @@ class HomePage extends StatelessWidget {
                               'Track Progress',
                               style: TextStyle(
                                 color: Color(0xFF6C63FF),
-                                fontSize: 14,
+                                fontSize: 9,
                               ),
                             ),
                           ],
@@ -135,9 +135,143 @@ class HomePage extends StatelessWidget {
                   ],
                 ),
               ),
+              const SizedBox(height: 20),
+
+              const Text(
+                'Categories',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 15),
+
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  _buildCategoryCard(
+                    'Cardio',
+                    Icons.favorite,
+                    Colors.redAccent,
+                  ),
+
+                  _buildCategoryCard(
+                    'Strength',
+                    Icons.fitness_center,
+                    Colors.blue,
+                  ), 
+
+                  _buildCategoryCard(
+                    'Yoga',
+                    Icons.self_improvement,
+                    Colors.green,
+                  ),
+
+                  _buildCategoryCard(
+                    'All',
+                    Icons.grid_view,
+                    Colors.purple,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30),
+
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Today\'s Mission',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                   Text(
+                    'See All',
+                    style: TextStyle(
+                      color: Color(0xFF6c63FF),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
+
+              _buildWorkoutCard(
+                'Full Body WorkOut',
+                '45 min -Medium',
+                Icons.fitness_center,
+                0.68,
+              ),
+              const SizedBox(height: 15),
+
+              _buildWorkoutCard(
+                'Upper Body Strength',
+                '45 min -Intermediate',
+                Icons.accessibility_new,
+                0.45,
+              ),
+              const SizedBox(height: 15),
+
+              _buildWorkoutCard(
+                'Cardio Blast',
+                '20 min -High Intensity',
+                Icons.directions_run,
+                0.32,
+              ),
+
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildCategoryCard(String title, IconData icon, Color color) {
+    return Container(
+      width: 80,
+      padding: const EdgeInsets.all(10),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.1),
+            spreadRadius: 2,
+            blurRadius: 10,
+          ),
+        ],
+      ),
+
+      child: const Column(
+        children: [
+          const Container(
+            height: 40,
+            width: 40,
+            decoration: BoxDecoration(
+              color: color.withOpacity(0.2),
+              shape: BoxShape.circle,
+            ),
+            child: Center(
+              child: Icon(
+                icon,
+                color: color,
+                size: 22,
+              ),
+            ),
+          ),
+          SizedBox(height: 8),
+
+          Text(
+            'Title',
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+
       ),
     );
   }
