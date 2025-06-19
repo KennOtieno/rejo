bb7245e5a2467fefd406e297f431b6824b3598aa
KennOtieno <otiken001@gmail.com>
2025-06-19 06:30:33 -0700

    To be Continued

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -15,6 +15,70 @@ class DetailsPage extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
+
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite_border),        
+          ),
+
+        ],
+      ),
+
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            children: [
+              const Container(
+                height: 200,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xFF6c63FF),
+                      Color(0xFF5a54D1),
+                    ],
+                  ),
+                ),
+
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.fitness_center,
+                        color: Colors.white,
+                      ),
+
+                      const SizedBox(height: 15),
+
+                      Text(
+                        'Full Body Workout',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                       const SizedBox(height: 15),
+
+                      Text(
+                        '45 Min- 350 Cal',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.1),
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
 }
